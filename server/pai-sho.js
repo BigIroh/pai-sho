bc1f32912cc972495233ed68683e23bf3884dcab
Matthew Scandalis <scandalis.matthew@gmail.com>
2014-01-25 01:26:07 -0500

    reorganized pai-sho and added GET retrieval method for getting state

--- server/pai-sho.js
+++ server/pai-sho.js
@@ -1,13 +1,20 @@
 var route = require('koa-route');
+var mount = require('koa-mount');
 var parse = require('co-body');
+var koa = require('koa');
+
 
 module.exports = function(options) {
+	var app = koa();
+
 	var validate = options.validate || function*() {
 		return true;
 	};
+	
 	var gameDB = options.gameDB;
 	var playerDB = options.playerDB;
-	return route.post('/pai-sho', function*() {
+	
+	app.use(route.post('/', function*() {
 		var body = yield parse(this);
 		var player = yield playerDB.get(this.session.player);
 		player = JSON.parse(player);
@@ -25,5 +32,18 @@ module.exports = function(options) {
 		} else {
 			this.status = 205;
 		}
-	});
+	}));
+
+	app.use(route.get('/', function*() {
+		var player = yield playerDB.get(this.session.player);
+		player = JSON.parse(player);
+		if(player.games.indexOf(this.query.game) > -1) {
+			var game = JSON.parse(yield gameDB.get(this.query.game));
+			this.body = game.currentState;
+		} else {
+			this.status = 205;
+		}
+	}));
+
+	return mount('/pai-sho', app);
 };
